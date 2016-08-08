6709056de6a069f4785a17c649fb9c551aee9511
Takashi Masuyama <mamewotoko@gmail.com>
2016-08-08 20:17:37 +0900

    use credential file if it exists

--- oauth2/oauth2client/taskapi_test.py
+++ oauth2/oauth2client/taskapi_test.py
@@ -2,27 +2,35 @@ from oauth2client.client import flow_from_clientsecrets, credentials_from_client
 from apiclient.discovery import build
 import json, httplib2, sys
 from oauth2client.file import Storage
-import setting
+import setting, os
 
+credential_path = "../credentials.json"
 credentials = None
 
-#redirect_uri = 'http://localhost'
-redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
-flow = flow_from_clientsecrets(setting.CONFIG,
-                               scope='https://www.googleapis.com/auth/calendar',
-                               redirect_uri=redirect_uri)
-
-auth_uri = flow.step1_get_authorize_url()
-print "open the following URL by web browser"
-print auth_uri
-http = httplib2.Http()
-resp, content = http.request(auth_uri, method='GET')
-#print content
-print "enter code (string following \"code=\" of URL):"
-code = sys.stdin.readline().rstrip()
-credentials = flow.step2_exchange(code, http=http)
 storage = Storage(setting.STORAGE_FILE)
-storage.put(credentials)
+
+if os.path.isfile(setting.STORAGE_FILE):
+    credentials = storage.get()
+
+if credentials is None:
+    #redirect_uri = 'http://localhost'
+    redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+    flow = flow_from_clientsecrets(setting.CONFIG,
+                                   scope='https://www.googleapis.com/auth/calendar',
+                                   redirect_uri=redirect_uri)
+
+    auth_uri = flow.step1_get_authorize_url()
+    print "open the following URL by web browser"
+    print auth_uri
+    http = httplib2.Http()
+    resp, content = http.request(auth_uri, method='GET')
+    #print content
+    print "enter code (string following \"code=\" of URL):"
+    code = sys.stdin.readline().rstrip()
+    credentials = flow.step2_exchange(code, http=http)
+    storage.put(credentials)
+else:
+    http = httplib2.Http()
 
 http = credentials.authorize(http)
 
